34a8e2ad9a17476f6966946269bde9c877a4fba5
Tarik (Julio Martins) <38379044+nordael@users.noreply.github.com>
2022-08-22 21:40:33 -0300

    adding openCSV to project and creating DiscplinaModel class (#2)

---
+++ datalayer/src/main/java/models/aluno/AlunoModel.java
@@ -0,0 +1,15 @@
+package models.aluno;
+
+public class AlunoModel {
+    private String grr;
+    private String nome;
+    private String curso;
+    private String telefone;
+    private String email;
+//    private HistoricoModel historico;
+
+    public AlunoModel() {
+
+    }
+
+}
---
+++ datalayer/src/main/java/models/disciplina/DisciplinaTipo.java
@@ -0,0 +1,7 @@
+package models.disciplina;
+
+public enum DisciplinaTipo {
+    ELETIVA,
+    OBRIGATORIA,
+    OPTATIVA;
+}
