cd8ea79447354ce75cbb1f5b767bd4ca72856282
Rubens Zandomenighi Laszlo <rubens.zandom@gmail.com>
2022-09-13 11:14:08 -0300

    Adding function to change row background based on subject situation

--- presentationlayer/src/main/java/sysqb/views/disciplinas/DisciplinasView.java
+++ presentationlayer/src/main/java/sysqb/views/disciplinas/DisciplinasView.java
@@ -4,14 +4,16 @@ import controllers.disciplina.DisciplinaCsvParserContoller;
 import models.disciplina.DisciplinaModel;
 import java.util.List;
 import java.util.ArrayList;
-import javax.swing.table.AbstractTableModel;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
 
-public class DisciplinasView extends AbstractTableModel{
+public class DisciplinasView extends AbstractTableModel {
     private DisciplinaCsvParserContoller disciplinaCsvParserContoller;
     private List<Integer> fields;
     private List<DisciplinaModel> disciplinasDoPeriodo;// usamos como dados uma lista genérica de Disciplina
 
-    private int periodo; 
+    private int periodo;
 
     // constantes p/identificar colunas
     private final int NOME = 0;
@@ -57,34 +59,34 @@ public class DisciplinasView extends AbstractTableModel{
     public String getColumnName(int columnIndex) {
         return colunas[columnIndex];
     }
-    
-    public DisciplinasView(){
+
+    public DisciplinasView() {
         this.disciplinaCsvParserContoller = new DisciplinaCsvParserContoller();
-        this.fields = List.of(5,3,6,10,1,8,9);
+        this.fields = List.of(5, 3, 6, 10, 1, 8, 9);
         this.periodo = 1;
-        this.disciplinasDoPeriodo = getListaDisciplinas(this.periodo, 2019);  
+        this.disciplinasDoPeriodo = getListaDisciplinas(this.periodo, 2019);
     }
 
-    public DisciplinasView(int periodo, int grade){
+    public DisciplinasView(int periodo, int grade) {
         this.disciplinaCsvParserContoller = new DisciplinaCsvParserContoller();
-        this.disciplinasDoPeriodo = getListaDisciplinas(periodo, grade); 
+        this.disciplinasDoPeriodo = getListaDisciplinas(periodo, grade);
     }
 
-    private List<DisciplinaModel> getListaDisciplinas(int periodo, int grade){
-        List<DisciplinaModel> disciplinas = new ArrayList<DisciplinaModel>(); 
+    private List<DisciplinaModel> getListaDisciplinas(int periodo, int grade) {
+        List<DisciplinaModel> disciplinas = new ArrayList<DisciplinaModel>();
 
-        if (grade == 2011){
+        if (grade == 2011) {
             disciplinas = disciplinaCsvParserContoller.csvReader("disciplinas_2011.csv", this.fields);
-        }else{
+        } else {
             disciplinas = disciplinaCsvParserContoller.csvReader("disciplinas_2019.csv", this.fields);
         }
 
-        List<DisciplinaModel> disciplinasPaginadas = new ArrayList<DisciplinaModel>(); 
+        List<DisciplinaModel> disciplinasPaginadas = new ArrayList<DisciplinaModel>();
 
         for (DisciplinaModel disciplina : disciplinas) {
-            
-            if (disciplina.getPeriodo() == periodo){
-                disciplinasPaginadas.add(disciplina); 
+
+            if (disciplina.getPeriodo() == periodo) {
+                disciplinasPaginadas.add(disciplina);
             }
         }
 
@@ -93,11 +95,11 @@ public class DisciplinasView extends AbstractTableModel{
 
     // funcao para lidar com mudanca de periodo
     public void handlePeriodChange(int periodo, int grade) {
-        this.disciplinasDoPeriodo = getListaDisciplinas(periodo, grade); 
+        this.disciplinasDoPeriodo = getListaDisciplinas(periodo, grade);
         fireTableDataChanged();
     }
 
-    public void printDisciplinas(){
+    public void printDisciplinas() {
         List<DisciplinaModel> disciplinas = disciplinaCsvParserContoller.csvReader("disciplinas_2019.csv", this.fields);
         for (DisciplinaModel disciplina : disciplinas) {
             System.out.println(disciplina.getCodigo() + " - " + disciplina.getNome());
--- presentationlayer/src/main/java/sysqb/views/guiPrincipal/GuiPrincipal.java
+++ presentationlayer/src/main/java/sysqb/views/guiPrincipal/GuiPrincipal.java
@@ -1,18 +1,21 @@
 package sysqb.views.guiPrincipal;
 
+import sysqb.views.historico.HistoricoView;
+import models.historico.HistoricoModel;
+import models.historico.SituacaoTipo;
+import models.historico.HistoricoDisciplinaModel;
+
+import java.util.*;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
- 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.table.*;
 
 import sysqb.views.disciplinas.DisciplinasView;
 
@@ -20,6 +23,8 @@ public class GuiPrincipal extends JFrame{
  
     private JTable table=new JTable();
     private DisciplinasView tableModel;
+
+    private Hashtable<String, HistoricoDisciplinaModel> disciplinasCursadasDic = new Hashtable<String, HistoricoDisciplinaModel>();
     
     private JButton btnNext=new JButton("Próximo período");
     private JButton btnPrev=new JButton("Período anterior");
@@ -42,7 +47,9 @@ public class GuiPrincipal extends JFrame{
  
         //neste ponto é criado o modelo da JTable
         tableModel=new DisciplinasView();
- 
+        HistoricoView historicoView = new HistoricoView();
+        setDisciplinasCursadasDic(historicoView.getListaMateriasHistorico());
+        
         //setando propriedades da tabela...
         //modelo
         table.setModel(tableModel);
@@ -52,7 +59,38 @@ public class GuiPrincipal extends JFrame{
         table.setFillsViewportHeight(true);
         //selecionar somente uma linha
         table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        
+
+        JTable table = new JTable(tableModel) {
+
+            Hashtable<String, HistoricoDisciplinaModel> dicionarioMaterias = getDicionarioMateriasCursadas();
+            public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+                Component c = super.prepareRenderer(renderer, row, column);
+                String codigoDisci = (String) getValueAt(row, 2); 
+               
+                if (dicionarioMaterias.containsKey(codigoDisci)){
+                    HistoricoDisciplinaModel disciplina = dicionarioMaterias.get(codigoDisci);
+                    SituacaoTipo situacao = disciplina.getSituacao();
+
+                    if (situacao.getSituacao().equals("Aprovado")){
+                        c.setBackground(Color.green);
+                    }else if (situacao.getSituacao().equals("Reprovado por nota")){
+                        c.setBackground(Color.red);
+                    }else if (situacao.getSituacao().equals("Reprovado por frequência")){
+                        c.setBackground(Color.red);
+                    }else if(situacao.getSituacao().equals("Matrícula")){
+                        c.setBackground(Color.blue);
+                    }
+                }else{
+                    // se a disciplina nao esta no historico, entao nao foi cursada
+                    c.setBackground(Color.gray);
+                }
+                
+                return c;
+            }
+        };
+    
+        table.setPreferredScrollableViewportSize(table.getPreferredSize());
+        table.changeSelection(1, 1, false, false);
         
         //add a table ao scroll pane
         JScrollPane scrollPane=new JScrollPane(table);
@@ -65,6 +103,16 @@ public class GuiPrincipal extends JFrame{
         //add botões ao JFrame
         this.add(btnNext);
         this.add(btnPrev);
+    }
+
+    public void setDisciplinasCursadasDic(List<HistoricoDisciplinaModel> listaDisciplinasHist){
+        for (HistoricoDisciplinaModel disciplinaHistorico : listaDisciplinasHist) {
+            disciplinasCursadasDic.put(disciplinaHistorico.getCodigoDisciplina(), disciplinaHistorico);
+        }
+    }
+
+    public Hashtable<String, HistoricoDisciplinaModel> getDicionarioMateriasCursadas(){
+        return this.disciplinasCursadasDic; 
     }
 
     public int getPeriodo() {
